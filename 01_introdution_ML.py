7583bd5df9df591c9603b43b0f6652480c28a42f
Tayyab Malik <mtayyabmalik99@gmail.com>
2021-09-25 21:50:49 +0500

    1.0

---
+++ 01_introdution_ML.py
@@ -0,0 +1,31 @@
+# (01)*********ML Algo introduction**************
+
+# ----Artificial inteligance
+# Any technique which enables computers to mimic human behavior.
+
+# ----Machine Learning
+# Subset of AI techniques whic use statistical methods to enable machines to improve with experience
+
+# ----Deep Learning
+# Subset of ML which make the computation of muli_layer neural networks feasible 
+
+
+
+
+# ----Artificial intelligance subfields
+# (1)Machine Learning
+# ..Deep learning
+# ..Predictive Analytics
+# (2)Netural Language processing(NLP)
+# ..Translation
+# ..Classification & clustering
+# ..Information Extraction
+# (3)Speech
+# ..Speech to Text
+# ..Text to Speech
+# (4)Expert Systems
+# (5)Planning,Scheduling & Optimization
+# (6)Robotics
+# (7)Vision
+# ..Image Recognition
+# ..Machine Vision
